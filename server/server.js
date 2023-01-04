bc36918693a0872c1f86bfbd99c87e007274c258
Matt Teixeira <matt1teixeira@gmail.com>
2023-01-04 18:14:50 -0500

    Set to production

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 app.use(require("./routes"));
 
 // Serve frontend
-if ((process.env.NODE_ENV = "production")) {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) => {
