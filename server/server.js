22f52f772a9be92d25922e2127ff03f4ca880c0d
Matt Teixeira <matt1teixeira@gmail.com>
2023-01-04 18:34:33 -0500

    New Build

--- server/server.js
+++ server/server.js
@@ -6,8 +6,7 @@ const PORT = process.env.PORT || 5000;
 const path = require("path");
 const cors = require("cors");
 
-const coresOpts = {origin: 'https://tiny-e-comms.herokuapp.com/'}
-app.use(cors(coresOpts));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.static("public"));
