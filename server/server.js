6a937e9d3f1e65a526f874bd01ee66501d521c3c
Matt Teixeira <matt1teixeira@gmail.com>
2023-01-04 18:39:07 -0500

    New Build

--- server/server.js
+++ server/server.js
@@ -6,21 +6,20 @@ const PORT = process.env.PORT || 5000;
 const path = require("path");
 const cors = require("cors");
 
-app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.static("public"));
-
+app.use(cors());
 
 app.use(require("./routes"));
 
 // Serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if ((process.env.NODE_ENV = "production")) {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(
-      path.resolve(__dirname, "client", "build", "index.html")
+      path.resolve(__dirname, "../", "client", "build", "index.html")
     );
   });
 }
