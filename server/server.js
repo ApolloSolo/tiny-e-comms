3e5574882ea20440e24a82c0c44c5a3248365556
Matt Teixeira <matt1teixeira@gmail.com>
2023-01-04 17:20:38 -0500

    Front build

--- server/server.js
+++ server/server.js
@@ -4,9 +4,12 @@ const app = express();
 const db = require("./db/connection");
 const PORT = process.env.PORT || 5000;
 const path = require("path");
+const cors = require("cors");
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.static("public"));
+app.use(cors());
 
 app.use(require("./routes"));
 
