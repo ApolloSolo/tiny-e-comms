fa241249fbd1fda19232f0e2a2ff1316956295b3
Matt Teixeira <matt1teixeira@gmail.com>
2023-01-04 16:48:35 -0500

    Increased JWT expire time

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 async function generateToken({ username, _id }) {
   const payload = { username, _id };
   return jwt.sign({ data: payload }, process.env.JWT_SECRET, {
-    expiresIn: "12hr",
+    expiresIn: "24hr",
   });
 }
 
