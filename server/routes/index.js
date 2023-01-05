578fb2f526aebd506e43732fc8eb3e06822dbb20
Matt Teixeira <matt1teixeira@gmail.com>
2023-01-05 09:04:05 -0500

    Comment

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const apiRoutes = require('./api');
 const paymentRoute = require("./payment");
 
 router.use('/api', apiRoutes);
-router.use('/process', paymentRoute)
+//router.use('/process', paymentRoute)
 
 router.use((req, res) => {
     res.status(404).send('<h1>😝 404 Error!</h1>');
