70f36e8403e3b4664ecade7e678962b36d8d3032
Matt Teixeira <matt1teixeira@gmail.com>
2023-01-07 18:57:09 -0500

    Removed 404 route

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,7 @@ const apiRoutes = require('./api');
 const paymentRoute = require("./payment");
 
 router.use('/api', apiRoutes);
-//router.use('/process', paymentRoute)
+router.use('/process', paymentRoute)
 
-router.use((req, res) => {
-    res.status(404).send('<h1>😝 404 Error!</h1>');
-  });
 
 module.exports = router
